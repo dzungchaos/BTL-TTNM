e2d59996088b91d27bcd6de768a0efa63cd2e53c
Hung.VS <HungICT2407@gmail.com>
2021-11-20 12:01:41 +0700

    [WIP]: living room

--- src/components/vrt/Overview.js
+++ src/components/vrt/Overview.js
@@ -1,28 +1,4 @@
-
-import { useState, useEffect } from 'react';
-
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
-
-const useWindowDimensions = () => {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return windowDimensions;
-}
+import {useWindowDimensions} from "../../utils"
 
 
 function Overview(props) {
--- src/components/vrt/apartment/LivingRoom.js
+++ src/components/vrt/apartment/LivingRoom.js
@@ -1,5 +1,18 @@
+import {useWindowDimensions} from "../../../utils"
+
 function LivingRoom(props) {
-  return <h1>Hello, LivingRoom{props.name}</h1>;
+
+
+  const { height: h, width: w} = useWindowDimensions();
+
+     const styles = {
+	  width: w,
+	  height: h 
+    };
+
+  return <iframe style={styles}  frameBorder="0"
+				src="https://momento360.com/e/u/b38b5296c8264f8f915d14f57a27441a?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium"
+			></iframe>
 }
 
 export default LivingRoom;
