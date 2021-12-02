b2dea9943d42f1052e1fc2bab4169f7fe84ae197
Hung.VS <HungICT2407@gmail.com>
2021-12-02 17:33:32 +0700

    [VRT - OVERVIEW]: styling OVERVIEW button

--- src/components/vrt/Vrt.js
+++ src/components/vrt/Vrt.js
@@ -1,52 +1,42 @@
 // https://www.youtube.com/watch?v=5lrLlcoRFKI
 
-
-
-import { Outlet } from 'react-router';
-import { Link } from 'react-router-dom';
-
+import { Outlet } from "react-router";
+import { Link } from "react-router-dom";
 
 export function Vrt(props) {
-//   return <h1>Hello, {props.name}</h1>;
+  //   return <h1>Hello, {props.name}</h1>;
 
+  // const menuBottomStyles={
 
+  // 	height: "10%",
+  // position: "absolute",
+  // width: "80%",
+  // "margin-left": "10%", /** width: "80%" and "margin-left": "10%" centers the menu. */
+  // "background-color": "",
+  // "margin-top": "-10%"
+  // };
 
-		// const menuBottomStyles={
-			
-		// 	height: "10%",
-    // position: "absolute",
-    // width: "80%",
-    // "margin-left": "10%", /** width: "80%" and "margin-left": "10%" centers the menu. */
-    // "background-color": "",
-    // "margin-top": "-10%"
-		// };
-	
-		return <html>
-		<head>
-			<title>Virtual tour</title>
-		</head>	
-		<body>
-
-		<Link to="overview">OVERVIEW</Link> 
-
-		</body> 
-
-<Apartment>
-
-</Apartment>
-
-<Link to="map">MAP</Link>
-<Link to="/vrt">VRT</Link>
-
-		<Outlet/>
-	</html>
+  return (
+    <div>
+      <Link class="float" to="overview">OVERVIEW</Link>
+ 
+      {/* Comment here 
+	  <Apartment class="float"></Apartment>
+	  <Link class="float" to="map">MAP</Link>
+      <Link class="float" to="/vrt">VRT</Link>
+      
+	  */}
+	  <Outlet />
+    </div>
+  );
 }
 
 const Apartment = (props) => {
-return <ul>
-  <li>
-APARTMENT	
-	</li>
+  return (
+    <ul>
+      <li>APARTMENT</li>
+
+      {/* Comment here 
   <li><a href="/vrt/apartment/living-room">
 	Living room
 	</a></li>
@@ -61,9 +51,7 @@ Bedroom
 
 Bathroom
 	</a></li>
-</ul>
-}
-
-
-
-
+*/}
+    </ul>
+  );
+};
