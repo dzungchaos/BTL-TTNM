4a631be48bea380ee595620f3abc3b1de4931f5a
dzungchaos <78160984+dzungchaos@users.noreply.github.com>
2021-12-29 20:26:03 +0700

    Delete index.js

--- src/components/home/Support/res/index.js
+++
@@ -1,4 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faEnvelope, faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faHome, faEnvelope, faPhone, faGlobe)
